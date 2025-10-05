0ad116bbe8491df552f9b819125aeda4d374029c
Vidyesh Churi <vidyesh95@gmail.com>
2025-10-05 16:28:54 +0530

    feature:ui

--- components/landing_page/CTASection.tsx
+++ components/landing_page/CTASection.tsx
@@ -3,15 +3,17 @@ import { Button } from "../ui/button";
 
 export default function CTASection() {
   return (
-    <section className="container mx-auto px-4 py-16">
+    <section className="mx-auto max-w-5xl px-4 py-16">
       <div className="bg-gradient-to-r from-card to-card-foreground rounded-3xl p-12 text-center">
-        <h2 className="text-4xl md:text-5xl font-bold mb-4">
-          Ready to Transform Your Content?
-        </h2>
-        <p className="text-xl mb-8 text-purple-100">
-          Join thousands of creators making viral-worthy videos
-        </p>
-        <Button>Start Creating Free</Button>
+        <hgroup>
+          <h2 className="text-4xl md:text-5xl font-bold mb-4">
+            Ready to Transform Your Content?
+          </h2>
+          <p className="text-xl mb-8 text-white">
+            Join thousands of creators making viral-worthy videos
+          </p>
+        </hgroup>
+        <Button className="cursor-pointer">Start Creating Free</Button>
       </div>
     </section>
   );
--- components/landing_page/Features.tsx
+++ components/landing_page/Features.tsx
@@ -27,7 +27,7 @@ const features = [
 
 export default function Features() {
   return (
-    <section className="container mx-auto px-4 py-16">
+    <section className="mx-auto px-4 py-16 max-w-5xl">
       <div className="text-center mb-16">
         <h2 className="text-4xl md:text-5xl font-bold mb-4">Everything You Need to Go Viral</h2>
         <p className="text-xl text-slate-400">Professional video editing, simplified</p>
